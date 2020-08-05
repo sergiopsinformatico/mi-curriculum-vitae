54ad98689211bb26c812e825d5fb4d0d7a1b063a
Sergio Pérez Sánchez <sergiopsinformatico@gmail.com>
2020-08-06 00:13:53 +0200

    Página Web v.0

---
+++ src/app/main-profile/main-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-profile',
+  templateUrl: './main-profile.component.html',
+  styleUrls: ['./main-profile.component.css']
+})
+export class MainProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
